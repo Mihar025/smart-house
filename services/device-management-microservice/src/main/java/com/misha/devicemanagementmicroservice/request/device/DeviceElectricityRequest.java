aa9d467b44052eb1be45cbb6ad3914522eb38b05
Mishar025 <mishamay583@gmail.com>
2024-08-27 21:08:57 -0400

    Discovery-Microservice

---
+++ services/device-management-microservice/src/main/java/com/misha/devicemanagementmicroservice/request/device/DeviceElectricityRequest.java
@@ -0,0 +1,11 @@
+package com.misha.devicemanagementmicroservice.request.device;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DeviceElectricityRequest {
+}
