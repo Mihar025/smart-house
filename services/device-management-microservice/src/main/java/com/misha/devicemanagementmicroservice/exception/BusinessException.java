fc6187b38359ae32ab2fe8345465a3ddd5a8b098
Mishar025 <mishamay583@gmail.com>
2024-08-27 21:08:00 -0400

    Discovery-Microservice

---
+++ services/device-management-microservice/src/main/java/com/misha/devicemanagementmicroservice/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.misha.devicemanagementmicroservice.exception;
+
+public class BusinessException extends RuntimeException{
+    public BusinessException(String message) {
+        super(message);
+    }
+}
