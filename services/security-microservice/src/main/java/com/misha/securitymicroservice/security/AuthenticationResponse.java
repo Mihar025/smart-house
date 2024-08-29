c6390bdc7e9a1ad15f78c19436a5328821458caa
Mishar025 <mishamay583@gmail.com>
2024-08-28 21:02:37 -0400

    Hello from Spring Security !

---
+++ services/security-microservice/src/main/java/com/misha/securitymicroservice/security/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.misha.securitymicroservice.security;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AuthenticationResponse {
+
+    private String token;
+}
