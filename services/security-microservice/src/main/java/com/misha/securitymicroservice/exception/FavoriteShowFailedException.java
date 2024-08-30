004647d044d8ff584f62de5de939c05deb4efcf8
Mishar025 <mishamay583@gmail.com>
2024-08-30 18:52:01 -0400

    Discovery-Microservice

---
+++ services/security-microservice/src/main/java/com/misha/securitymicroservice/exception/FavoriteShowFailedException.java
@@ -0,0 +1,7 @@
+package com.misha.securitymicroservice.exception;
+
+public class FavoriteShowFailedException extends RuntimeException {
+    public FavoriteShowFailedException(String message) {
+        super(message);
+    }
+}
