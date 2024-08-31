6791fc54b776aaf87d2ebf4798131fd9cf123cd4
Mishar025 <mishamay583@gmail.com>
2024-08-31 18:48:45 -0400

    Discovery-Microservice

---
+++ services/security-microservice/src/main/java/com/misha/securitymicroservice/exception/GetAllOrderException.java
@@ -0,0 +1,7 @@
+package com.misha.securitymicroservice.exception;
+
+public class GetAllOrderException extends RuntimeException {
+    public GetAllOrderException(String msg) {
+        super(msg);
+    }
+}
