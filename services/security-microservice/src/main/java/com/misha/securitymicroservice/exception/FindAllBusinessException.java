76e9478e460c1033b417b4504e0e322da86b9ecc
Mishar025 <mishamay583@gmail.com>
2024-08-31 18:48:36 -0400

    Discovery-Microservice

---
+++ services/security-microservice/src/main/java/com/misha/securitymicroservice/exception/FindAllBusinessException.java
@@ -0,0 +1,7 @@
+package com.misha.securitymicroservice.exception;
+
+public class FindAllBusinessException extends RuntimeException {
+    public FindAllBusinessException(String msg) {
+        super(msg);
+    }
+}
